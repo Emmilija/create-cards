6eb0fe84c456a63cf0d14bd4ee5b37f85761987e
Emilija <karatashevskaemilija@gmail.com>
2024-02-19 14:56:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
+import "./reset.css"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Your Cards</h1>
+      <p>Add, edit or delete your cards any time</p>
       </header>
     </div>
   );
