19c68ba2c2da93136ba75e977aa80f2b1ec4460e
EmilijaK-Codedesk <emilijak@codedesk.mk>
2024-02-21 10:54:33 +0100

    add form

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "../styles/index.css"
+import Button from "./Button";
 
 
 export default function CardForm({onAddCard}) {
@@ -31,59 +32,128 @@ setCardInfo({
 
 
     return(
-        <div className="modal"> 
-        <div className="modal-container">
-        <h1>Add your card details</h1>
-
-<form onSubmit={handleSubmit} className="card-form">
-           <label htmlFor="name">Name in card</label>
-           <input 
-               type="text" 
-               id="name" 
-               name="name" 
-               value={cardInfo.name} 
-               onChange={handleChange} 
-               required 
-           />
-
-           <label htmlFor="number">Card number</label>
-           <input 
-               type="text" 
-               id="number" 
-               name="number" 
-               value={cardInfo.number} 
-               onChange={handleChange} 
-               required 
-           />
-
-           <label htmlFor="expiry">Expiry date</label>
-           <input 
-               type="text" 
-               id="expiry" 
-               name="expiry" 
-               value={cardInfo.expiry} 
-               onChange={handleChange} 
-               required 
-           />
-
-           <label htmlFor="cvv">CVC(Security code)</label>
-           <input 
-               type="text" 
-               id="cvv" 
-               name="cvv" 
-               value={cardInfo.cvv} 
-               onChange={handleChange} 
-               required 
-           />
-
-           <button className="add-card-but" type="submit">Confirm</button>
-       </form>
-        </div>
-       
-        </div>
+<div className="relative w-full">
+    <h1>Add your card details</h1>
     
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <div className="mb-4 flex-column">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Name in the card
+          </label>
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            id="name"
+            placeholder=" Jane Doe"
+            aria-label="Full name"
+            name="name"
+            value={cardInfo.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="number">
+            Card number
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="number"
+            type="text"
+            placeholder=" **** **** **** ****"
+            name="number"
+            value={cardInfo.number}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="number">
+            Expiry date
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="expiry"
+            type="expiry"
+            placeholder=" **/**"
+            name="expiry"
+            value={cardInfo.expiry}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="number">
+            CVC (security code)
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="cvv"
+            type="number"
+            placeholder="***"
+            name="cvv"
+            value={cardInfo.cvv}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+ <Button/>
+     
+    </div>
     )
 }
 
 
 
+
+
+// <div className="modal"> 
+// <div className="modal-container">
+// <h1>Add your card details</h1>
+
+// <form onSubmit={handleSubmit} className="card-form">
+//    <label htmlFor="name">Name in card</label>
+//    <input 
+//        type="text" 
+//        id="name" 
+//        name="name" 
+//        value={cardInfo.name} 
+//        onChange={handleChange} 
+//        required 
+//    />
+
+//    <label htmlFor="number">Card number</label>
+//    <input 
+//        type="text" 
+//        id="number" 
+//        name="number" 
+//        value={cardInfo.number} 
+//        onChange={handleChange} 
+//        required 
+//    />
+
+//    <label htmlFor="expiry">Expiry date</label>
+//    <input 
+//        type="text" 
+//        id="expiry" 
+//        name="expiry" 
+//        value={cardInfo.expiry} 
+//        onChange={handleChange} 
+//        required 
+//    />
+
+//    <label htmlFor="cvv">CVC(Security code)</label>
+//    <input 
+//        type="text" 
+//        id="cvv" 
+//        name="cvv" 
+//        value={cardInfo.cvv} 
+//        onChange={handleChange} 
+//        required 
+//    />
+
+//    <button className="add-card-but" type="submit">Add Card</button>
+// </form>
+// </div>
+
+// </div>
